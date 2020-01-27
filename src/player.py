3193416fbfe3d79569d0a3c552603d966c7d4f87
BryanValerio <58003981+BryanValerio@users.noreply.github.com>
2020-01-26 21:36:44 -0500

    Update player.py

--- src/player.py
+++ src/player.py
@@ -6,13 +6,14 @@ vec = pygame.math.Vector2
 
 
 class Player:
-    def __init__(self, app, position):
+    def __init__(self, base, position):
         self.base = base
         self.grid_position = position
         self.pix_position = self.get_pix_position()
         # where Pac-Man starts relative to the board
         self.direction = vec(1, 0)
         self.stored_direction = None
+        self.able_to_move = True
 
 
     def get_pix_position(self):
@@ -22,10 +23,12 @@ class Player:
 
 
     def update(self):
-        self.pix_position += self.direction
+        if self.able_to_move:
+            self.pix_position += self.direction
         if self.time_to_move():
             if self.stored_direction != None:
                 self.direction == self.stored_direction
+            self.able_to_move = self.can_move()
         # calls to the next function in order to check that the player is within bounds  
 
         self.grid_position[0] = (self.pix_position[0]-top_bottom_space+self.base.cell_width//2)//self.base.cell_width+1
@@ -40,14 +43,20 @@ class Player:
         if int(self.pix_position.y+top_bottom_space//2) % self.base.cell_height == 0:
             if self.direction == vec(0, 1) or self.direction == vec(0, -1):
                 return True
-    # to make the player stay within the bounds
+        # to make the player stay within the bounds
 
+    def can_move(self):
+        for wall in self.base.walls:
+            if vec(self.grid_position+self.direction) == wall:
+                return False  
+        return True     
 
     def draw(self):
         pygame.draw.circle(self.base.screen, (190, 190, 15), (int(self.pix_position.x), int(self.pix_position.y)), self.base.cell_width//2-2)
         # the controllable thing
-        pygame.draw.rect(self.base.screen, (255, 0, 0), (self.grid_position[0]*self.base.cell_width+top_bottom_space//2, self.grid_position[1]*self.base.cell_height+top_bottom_space//2, self.base.cell_width, self.base.cell_height), 1)
-        # the reactangle keeping track of the player
+        
+        # pygame.draw.rect(self.base.screen, (255, 0, 0), (self.grid_position[0]*self.base.cell_width+top_bottom_space//2, self.grid_position[1]*self.base.cell_height+top_bottom_space//2, self.base.cell_width, self.base.cell_height), 1)
+        # ^^ the reactangle keeping track of the player
 
 
     def move(self, direction):
