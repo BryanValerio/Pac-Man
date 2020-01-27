271038469deae03707d964c439860727fbac7e6c
BryanValerio <58003981+BryanValerio@users.noreply.github.com>
2020-01-26 23:24:57 -0500

    Update player.py

--- src/player.py
+++ src/player.py
@@ -16,17 +16,20 @@ class Player:
         self.stored_direction = None
         self.able_to_move = True
         self.current_score = 0
+        self.speed = 2
 
 
     def get_pix_position(self):
+        """Player's position relative to the board"""
         return vec((self.grid_position.x*self.base.cell_width)+top_bottom_space//2+self.base.cell_width//2, 
         (self.grid_position.y*self.base.cell_height)+top_bottom_space//2+self.base.cell_height//2)
     # where Pac-Man starts relative to the board
 
 
     def update(self):
+        """Updates the movements of the player and keeps track of where the player is"""
         if self.able_to_move:
-            self.pix_position += self.direction
+            self.pix_position += self.direction*self.speed
         if self.time_to_move():
             if self.stored_direction != None:
                 self.direction == self.stored_direction
@@ -42,18 +45,21 @@ class Player:
     
 
     def on_coin(self):
+        """Defines whether or not the player is on a coin and what happens"""
         if self.grid_position in self.base.coins:
             return True
         else:
             return False
 
     def eat_coin(self):
+        """Adds a point to the current score if the player is on a coin"""
         self.base.coins.remove(self.grid_position)
         self.current_score += 1
         
 
 
     def time_to_move(self):
+        """Determines whether or not the player is within the bounds of the maze"""
         if int(self.pix_position.x+top_bottom_space//2) % self.base.cell_width == 0:
             if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
                 return True
@@ -63,12 +69,14 @@ class Player:
         # to make the player stay within the bounds
 
     def can_move(self):
+        """Detects whether there is a wall in front of the player"""
         for wall in self.base.walls:
             if vec(self.grid_position+self.direction) == wall:
                 return False  
         return True     
 
     def draw(self):
+        """The player-controlled object"""
         pygame.draw.circle(self.base.screen, (190, 190, 15), (int(self.pix_position.x), int(self.pix_position.y)), self.base.cell_width//2-2)
         # the controllable thing
 
@@ -77,4 +85,5 @@ class Player:
 
 
     def move(self, direction):
+        """Movement stored for use is detecting collisions"""
         self.stored_direction = direction
