a1472fc19df917a262c057288fa13b0317303768
BryanValerio <58003981+BryanValerio@users.noreply.github.com>
2020-01-26 20:27:08 -0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
+from pygame.math import Vector2 as vec
+
 width, height = 610, 670
 maze_width, maze_height = 560, 520
 start_text_size = 16
@@ -7,5 +9,5 @@ FPS = 60
 # settings for the screen
 
 
-
-#settings for the enemy
+player_start_position = vec(1, 1)
+#settings for the player
