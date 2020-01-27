874e97e3164899f613a8bdc42bcd1767e7978bfd
BryanValerio <58003981+BryanValerio@users.noreply.github.com>
2020-01-26 19:09:14 -0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,11 @@
-width, height = 560, 620
+width, height = 610, 670
+maze_width, maze_height = 560, 520
 start_text_size = 16
 start_font = 'arialblack'
+top_bottom_space = 50
+FPS = 60
 # settings for the screen
 
-FPS = 60
 
-# settings for the color
+
 #settings for the enemy
